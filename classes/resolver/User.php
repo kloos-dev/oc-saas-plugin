777e1756450fa3ca0702f6e99cee370602bfa90d
Sebastiaan Kloos <sebastiaan@kloos.dev>
2021-03-03 16:50:02 +0100

    Added fix for User resolver

--- classes/resolver/User.php
+++ classes/resolver/User.php
@@ -1,13 +1,18 @@
 <?php namespace Kloos\Saas\Classes\Resolver;
 
 use Auth;
+use BackendAuth;
 use Kloos\Saas\Models\Tenant;
 
 class User
 {
     public function run($request)
     {
-        $user = Auth::getUser();
+        if ($request->is(config('cms.backendUri').'*')) {
+            $user = BackendAuth::getUser();
+        } else {
+            $user = Auth::getUser();
+        }
 
         if (!$user) {
             return null;
